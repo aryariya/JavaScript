07ac9672559d798808302c61e51d271e766a6c44
aryariya <aryariya2405@gmail.com>
2026-09-10 14:18:40 +0000

    data type

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 724030 // cant change
+let accountEmail = "riya23@gmail.accountEmail"
+var accountPassword = "23452733"
+accountCity = "Barabanki" 
+let accountState;// semicolon doesnot matter //output undefined
+
+// accountId = 2 // not allowed
+
+accountEmail= "arrfa2@gmail.com"
+accountPassword="123456"
+accountCity= "jaipur"         
+
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of block and functional scope
+
+*/
+
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3 + 3)// we are using node js, not browser
+
+console.log(3+3); console.log("riya") //console.log(3+3) console.log("riya")// this give error
+//code readability should be high
+
+let name="riya"
+let age=18
+let isLoggedIn= false
+let state=null
+
+/*
+number==> 2 to power 53
+bigint
+string==>""
+boolean==> true/ false
+null==>standalone value
+undefined
+symbol=> unique
+
+object
+ */
+console.log(typeof "riya");
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
